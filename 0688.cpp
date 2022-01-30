c4e48e1dd18f9cb927d75fcc76db6fd703ce8644
unknown <dde66zdx@icloud.com>
2022-01-30 16:38:40 +0900

    2022/01/30

---
+++ 0688.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<stack>
+#include<set>
+#include<bitset>
+#include<vector>
+#include<string>
+#include <iomanip>
+#include <deque>
+#include<list>
+#include<cmath>
+#include <sstream>
+#define rep(i,n) for(int i=0;i<(n);i++)
+using namespace std;
+using ll=long long;
+using P=pair<int,int>;
+using P_S=pair<int,string>;
+using P_D=pair<double,int>;
+using T=tuple<int,int,char,ll,string>;
+
+int main()
+{
+    int A,B;
+    cin>>A>>B;
+    cout<<max(A+B,A-B)<<endl;
+    cout<<min(A+B,A-B)<<endl;
+}
